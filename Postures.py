b3b39371f87d034cac1dcb380d562e4568862944
YoshikiHi <g1545196@cc.kyoto-su.ac.jp>
2017-12-19 10:47:20 +0900

    Posture Lesson. Sit and Stand

---
+++ Postures.py
@@ -0,0 +1,25 @@
+import time
+
+class MyClass(GeneratedClass):
+    def __init__(self):
+        GeneratedClass.__init__(self)
+        #ALProxy("API Name","RobotIPAddr",RobotPort)
+        self.posture = ALProxy("ALRobotPosture","localhost",56678)
+
+    def onLoad(self):
+        #put initialization code here
+        pass
+
+    def onUnload(self):
+        #put clean-up code here
+        pass
+
+    def onInput_onStart(self):
+        self.posture.post.goToPosture("Sit",1.0) 
+        time.sleep(1)
+        self.posture.post.goToPosture("Stand",1.0) 
+        pass
+
+    def onInput_onStop(self):
+        self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+        self.onStopped() #activate the output of the box
